cde28296531d8deee301547de994e8b832248949
Philip Smith <philip.smith@nordcloud.com>
2016-07-15 13:16:38 +0100

    add features to disable termination protection add IAM permissions for ASG manipulation

--- scripts/termcontrol.py
+++ scripts/termcontrol.py
@@ -12,13 +12,13 @@ def lambda_handler(event, context):
         print "Nothing to do, function disabled."
     else:
         rname = event['Region']
+        action = collections.defaultdict(list)
         ec2 = boto3.client('ec2', rname)
         reservations = ec2.describe_instances(
             Filters=[
                 {'Name': 'tag-key', 'Values': ['Scaling']},
-                {'Name': 'tag-value', 'Values': [fstate]},
             ])['Reservations']
-        
+
         instances = sum(
             [
                 [i for i in r['Instances']]
@@ -26,11 +26,28 @@ def lambda_handler(event, context):
             ], [])
 
         for instance in instances:
+            try:
+                value = [
+                    str(val.get('Value')) for val in instance['Tags']
+                    if val['Key'] == 'Scaling'][0]
+                action[value].append(instance['InstanceId'])
+            except:
+                action['other'].append(instance['InstanceId'])
 
-            instance = ec2.Instance(i['InstanceId'])
-            response = instance.modify_attribute(
-                DisableApiTermination={
-                    'Value': True
-                },
-            )
-            print "Termination Protection has been Enabled for: %s" % (i['InstanceId'])
+            for enableprotection in action['False']:
+                instance = ec2.Instance(enableprotection)
+                    response = instance.modify_attribute(
+                        DisableApiTermination={
+                            'Value': True
+                        },
+                    )
+                    print "Termination Protection has been Enabled for: %s" % (i['InstanceId'])
+
+            for disableprotection in action['True']:
+                instance = ec2.Instance(disableprotection)
+                    response = instance.modify_attribute(
+                        DisableApiTermination={
+                            'Value': False
+                        },
+                    )
+                    print "Termination Protection has been Disabled for: %s" % (i['InstanceId'])
